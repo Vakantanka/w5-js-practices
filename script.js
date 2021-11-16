86af21dbc924ccf8862d947befb6d0cd593b2b93
Vakantanka <laszlo.fenyvesi@gmail.com>
2021-11-16 09:36:28 +0100

    Monday

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function loadEvent() {
-
-    
-
-    console.log("Load completed.");
+function kulonbseg(firstNumber, secondNumber) {
+    if (firstNumber > secondNumber) {
+        console.log(firstNumber - secondNumber);
+    } else if (firstNumber < secondNumber) {
+        console.log(secondNumber - firstNumber);
+    } else {
+        console.log("A két szám egyenlő.");
+    }
 }
 
-window.addEventListener("load", loadEvent);
+kulonbseg(1234754, 56497);
